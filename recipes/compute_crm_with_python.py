f73c0bedc04d1367a3b2566d250e33dceb460376
admin <gonzalo.betegon@dataiku.com>
2021-08-06 13:28:50 -0400

    Recipe DKU_CUSTOMER_LIFETIME_VALUE.compute_crm_with_python saved through notebook notebook editor for compute_crm_with_python

--- recipes/compute_crm_with_python.py
+++ recipes/compute_crm_with_python.py
@@ -1,20 +1,18 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 crm_prepared = dataiku.Dataset("crm_prepared")
-crm_prepared_df = crm_prepared.get_dataframe()
+df = crm_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['dif_revenue'] = df['revenue'] - df.groupby(['gender']).transform('mean')['revenue']
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-crm_with_python_df = crm_prepared_df # For this sample code, simply copy input to output
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 crm_with_python = dataiku.Dataset("crm_with_python")
-crm_with_python.write_with_schema(crm_with_python_df)
+crm_with_python.write_with_schema(df)
